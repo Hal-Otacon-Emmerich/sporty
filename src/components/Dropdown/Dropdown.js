0bd255a6e1337778df1b048c8d52a906b07d453e
Igor Smetanin <nidken25@gmail.com>
2025-08-12 13:57:56 +0200

    click outside

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,8 +1,10 @@
-import {useState} from 'react'
+import {useState, useEffect, useRef} from 'react'
 import './Dropdown.css'
 
 export const Dropdown = ({filter, sportsType, setSportsType}) => {
     const [openDropdown, setOpenDropdown] = useState(false)
+    const dropdownRef = useRef(null);
+
     function renderFilter() {
         return Array.from(filter).map((type, index) => (
             <li className="dropdown-list__item" onClick={() => setSportsType(type)} key={`${index}-${type}`}>
@@ -13,8 +15,20 @@ export const Dropdown = ({filter, sportsType, setSportsType}) => {
     function onOpenDropdown() {
         setOpenDropdown(!openDropdown)
     }
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+        if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+            setOpenDropdown(false);
+        }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => document.removeEventListener("mousedown", handleClickOutside);
+    }, []);
+
     return (
-        <div className="dropdown" onClick={onOpenDropdown}>
+        <div ref={dropdownRef} className="dropdown" onClick={onOpenDropdown}>
             <span>{sportsType ? sportsType : "Select the filter"}</span>
             {openDropdown && (
                 <ul className="dropdown-list">
